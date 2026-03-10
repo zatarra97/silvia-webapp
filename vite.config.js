f669c0315241ed932434bfd6c377a78425f0f5b4
Emmanuele Carlone <emmanuele.carlone@runelab.it>
2026-03-10 20:12:42 +0100

    Corretto path per la pages

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,20 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
-import react from '@vitejs/plugin-react'
-import flowbiteReact from "flowbite-react/plugin/vite";
-import { readFileSync } from 'fs'
+import { defineConfig } from "vite"
+import tailwindcss from "@tailwindcss/vite"
+import react from "@vitejs/plugin-react"
+import flowbiteReact from "flowbite-react/plugin/vite"
+import { readFileSync } from "fs"
 
 // Leggi la versione dal package.json
-const packageJson = JSON.parse(readFileSync('./package.json', 'utf8'))
+const packageJson = JSON.parse(readFileSync("./package.json", "utf8"))
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/WeddingCut-Webapp/',
-  plugins: [react(), tailwindcss(), flowbiteReact()],
-  build: {
-    outDir: 'build'
-  },
-  define: {
-    __APP_VERSION__: JSON.stringify(packageJson.version)
-  }
+	base: "/siliva-webapp/",
+	plugins: [react(), tailwindcss(), flowbiteReact()],
+	build: {
+		outDir: "build",
+	},
+	define: {
+		__APP_VERSION__: JSON.stringify(packageJson.version),
+	},
 })
